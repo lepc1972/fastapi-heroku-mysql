a370101430c6aa109bff90a4bd4f90fb6ccbbcdf
lepc1972 <githublepc@gmail.com>
2021-08-22 23:39:48 -0500

    avances

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column
+from sqlalchemy.sql.sqltypes import Integer, String
 from config.db import meta, engine
 
 
